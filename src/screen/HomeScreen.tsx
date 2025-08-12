0de87431a28deea233a6a8fac0822b2d939c0ef5
lipi507 <lipikhatun@iut-dhaka.edu>
2025-08-12 11:01:36 +0600

    HomeScreen

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground,Image, } from "react-native";
 
 interface HomeScreenProps {
   onLoginPress: () => void;
@@ -8,36 +8,48 @@ interface HomeScreenProps {
 
 const HomeScreen: React.FC<HomeScreenProps> = ({ onLoginPress, onSignupPress }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Food Delivery!</Text>
+    <ImageBackground
+      source={require('./Images/Image1.png')}  
+      
+      style={styles.backgroundImage}
+      resizeMode="cover"  
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to Food Delivery!</Text>
 
-      <TouchableOpacity style={styles.button} onPress={onLoginPress}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={onLoginPress}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={[styles.button, styles.signupButton]} onPress={onSignupPress}>
-        <Text style={styles.buttonText}>Signup</Text>
-      </TouchableOpacity>
-
-    </View>
+        <TouchableOpacity style={[styles.button, styles.signupButton]} onPress={onSignupPress}>
+          <Text style={styles.buttonText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 20,
-    backgroundColor: "#f3eaeaff",
+    backgroundColor: "rgba(280, 275, 275, 0.9)",  
   },
   title: {
     fontSize: 28,
     fontWeight: "bold",
     marginBottom: 28,
     textAlign: "center",
+    color: "#000", 
   },
   button: {
-    backgroundColor: "#ed7214ff", // similar orange color like Signup button
+    backgroundColor: "#ed7214ff",
     height: 40,
     borderRadius: 6,
     justifyContent: "center",
@@ -47,7 +59,7 @@ const styles = StyleSheet.create({
     marginVertical: 8,
   },
   signupButton: {
-    backgroundColor: "#0a8ce9ff", // different color for signup, e.g. blue
+    backgroundColor: "#0a8ce9ff",
   },
   buttonText: {
     color: "#fff",
@@ -57,3 +69,4 @@ const styles = StyleSheet.create({
 });
 
 export default HomeScreen;
+
